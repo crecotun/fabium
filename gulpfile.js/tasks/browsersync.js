729a24f4dad08d6fed564b2a0d005d62e7dfb628
Krekotun <a.krekotun@gmail.com>
2016-05-04 16:54:06 +0300

    fix watch

--- gulpfile.js/tasks/browsersync.js
+++ gulpfile.js/tasks/browsersync.js
@@ -4,13 +4,12 @@ var
 
 	path = require('path'),
 	browserSync = require('browser-sync'),
-	bSync = browserSync.create(),
 	config = require('../config'),
 	consoleError = require('../utils/console_error');
 
-function sync() {
+function bSync(cb) {
 
-	return bSync.init({
+	browserSync({
 		minify: false,
 		injectChanges: true,
 		files: [
@@ -25,8 +24,8 @@ function sync() {
 			baseDir: path.join(__dirname, '/../../dist')
 		},
 		port: 3000
-	});
+	}, cb);
 
 }
 
-module.exports = sync;
+module.exports = bSync;
