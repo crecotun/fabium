048a2af10992904b02ea155e9521d728d4e326e3
Krekotun <a.krekotun@gmail.com>
2016-11-15 19:33:25 +0200

    - order of generators in menu - added part about generator in README - some stuff for demo

--- plopfile.js/index.js
+++ plopfile.js/index.js
@@ -3,9 +3,9 @@ var config = require('../gulpfile.js/config.json')
 module.exports = function (plop) {
 
 	plop.setGenerator('styles:component', require('./generators/styles/component.js')(config));
+	plop.setGenerator('templates:component', require('./generators/templates/component.js')(config))
+	plop.setGenerator('templates:page', require('./generators/templates/page.js')(config))
 	plop.setGenerator('styles:mixin', require('./generators/styles/mixin.js')(config))
 
-	plop.setGenerator('templates:page', require('./generators/templates/page.js')(config))
-	plop.setGenerator('templates:component', require('./generators/templates/component.js')(config))
 
 };
